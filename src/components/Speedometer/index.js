4cfe3813c42b0e25411a77bd4ed5ba067d749564
dnithinkumar <dnithinkumar11@gmail.com>
2024-11-02 12:12:21 +0000

    push speedometer

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,47 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  state = {speed: 0}
+
+  accelerate = () => {
+    const {speed} = this.state
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
+  }
+
+  applyBrake = () => {
+    const {speed} = this.state
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  render() {
+    const {speed} = this.state
+    return (
+      <div className="bg-container">
+        <h1 className="main-heading">SPEEDOMETER</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          alt="speedometer"
+          className="image"
+        />
+        <h1 className="heading">Speed is {speed}mph</h1>
+        <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
+        <div className="button-container">
+          <button className="accelerate-button" onClick={this.accelerate}>
+            Accelerate
+          </button>
+          <button className="brake-button" onClick={this.applyBrake}>
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
